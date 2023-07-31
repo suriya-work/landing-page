4ceb4d37b15917ba9af85fdc9a782e452bd26890
suriya babaei <suriyalifework60@gmail.com>
2023-07-31 07:51:00 -0700

    create commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
 import React from 'react'
-
+import './App.css'
+// header 
+import Header from './components/header/Header';
+// banner
+import Banner from './components/banner/Banner';
 const App = () => {
   return (
     <div>
-     
+      <Header />
+      <Banner />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'white': '#ffffff',
+      'purple': '#5D50C6',
+      'orange': '#FF5722',
+      'bubble-gum': '#F85E9F',
+      'midnight': '#121063',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bermuda': '#78dcca',
+    },
     extend: {},
   },
   plugins: [],
