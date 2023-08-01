73662dfcb79cc35be46400cd2b5dc6eb8c3ba31b
suriya babaei <suriyalifework60@gmail.com>
2023-08-01 01:36:53 -0700

    change header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -43,7 +43,7 @@ export default function Header() {
                                     />
                                 </div>
                                 <div className="hidden sm:ml-6 sm:block">
-                                    <div className="flex space-x-4">
+                                    <div className="flex space-x-2">
                                         {navigation.map((item) => (
                                             <a
                                                 key={item.name}
@@ -69,7 +69,7 @@ export default function Header() {
                                         <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                             <span className="absolute -inset-1.5" />
                                             <span className="sr-only">Open user menu</span>
-                                        <a href="#" className='singup'>Sign Up</a>
+                                            <a href="#" className='singup text-white'>Sign Up</a>
                                             {/* <img
                                                 className="h-8 w-8 rounded-full"
                                                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
