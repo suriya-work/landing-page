f1f5079d95101fade5ac4fe042f28d543f4c78ea
suriya babaei <suriyalifework60@gmail.com>
2023-08-03 04:50:36 -0700

    travel

--- src/components/travel/Travel.jsx
+++ src/components/travel/Travel.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import myproject from '../../../public/images/Group 9238.png'
+import myproject from '../../../public/images/Group 9238.png';
+// number
+import Number from './Number';
 const Travel = () => {
     return (
-        <div className='container mx-auto px-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
-            <div className='pt-[6rem]'>
-                <img className='my-img' src={myproject} alt="my" />
+        <div className='container  grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 '>
+            <div className='pt-[6rem] '>
+                <img src={myproject} alt="my" />
             </div>
-            <div className='pt-[6rem] px-[6rem]'>
-                <h3 className='text-travel'>Travel Point</h3>
-                <h3 className='text-black font-bold text-[30px]'>We helping you find your dream location</h3>
-                <p className='text-gray text-sm pt-8'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC.</p>
+            <div className='pt-[3rem] px-[7rem]  travel-pon'>
+                <div className='pt-[2rem]'>
+
+                    <h3 className='text-travel pb-3'>Travel Point</h3>
+                    <h3 className='text-black font-bold text-[30px] '>We helping you find your dream location</h3>
+                </div>
+                <p className='text-gray text-sm pb-7'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC.</p>
+                <Number />
             </div>
+            {/* <div> */}
+            {/* </div> */}
 
         </div>
     )
