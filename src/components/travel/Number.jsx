9b8c7e8641863975ae2d621c7562b8534be5d1e7
suriya babaei <suriyalifework60@gmail.com>
2023-08-03 04:50:27 -0700

    number

---
+++ src/components/travel/Number.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+const number = [
+    { id: 1, num: '500k+', title: 'Holiday Package' },
+    { id: 2, num: '100k+', title: 'Luxury Hotel' },
+    { id: 3, num: '7k+', title: 'Premium Airlines' },
+    { id: 4, num: '2k+', title: 'Happy Customer' },
+]
+const Number = () => {
+    return (
+        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg-grid-cols-2 gap-7 pt-[2rem]'>
+            
+            {/* <div> */}
+                {
+                    number.map((numbers) => {
+                        return (
+                            <div key={numbers.id}  className='text-num'>
+                                <span className='text-orange font-bold '>{numbers.num}</span>
+                                <h3 className='text-black'>{numbers.title}</h3>
+                            </div>
+                        )
+                    })
+                }
+            {/* </div> */}
+        </div>
+    )
+}
+
+export default Number
