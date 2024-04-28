f9b9993941112cb0c2b49315ee0e7154cc4da6b4
suriya babaei <suriyalifework60@gmail.com>
2024-04-28 13:17:07 +0330

    change tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,28 @@
-/** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      'white': '#ffffff',
-      'purple': '#5D50C6',
-      'orange': '#FF5722',
-      'bubble-gum': '#F85E9F',
-      'midnight': '#121063',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bermuda': '#78dcca',
-      'gray': 'rgba(25, 24, 37, 0.50)',
-      'yellow' : '#FACD49',
-      'pink' : 'rgba(250, 205, 73, 0.08)',
-  
+    extend: {
+      colors: {
+        'white': '#ffffff',
+        'purple': '#5D50C6',
+        'orange': '#FF5722',
+        'bubble-gum': '#F85E9F',
+        'midnight': '#121063',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bermuda': '#78dcca',
+        'gray': 'rgba(25, 24, 37, 0.50)',
+        'yellow': '#FACD49',
+        'pink': 'rgba(250, 205, 73, 0.08)',
+      },
+      container: {
+        center: true,
+      },
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
-
